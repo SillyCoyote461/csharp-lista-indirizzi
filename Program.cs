9de2589c7334449b125a041a5a451ab78411b21e
Francesco C <fran461j@outlook.it>
2023-03-24 15:03:27 +0100

    update

--- Program.cs
+++ Program.cs
@@ -1,2 +1,50 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using var file = File.OpenText("..\\..\\..\\addresses.csv");
+string? line = file.ReadLine();
+
+//take out first stream from the loop
+Console.WriteLine(line);
+
+List<User> users = new List<User>();
+
+while (true)
+{
+    //reading and writing
+    line = file.ReadLine();
+    if (line == null) break;
+
+    var array = line.Split(',');
+    string name;
+    string surname;
+    string street;
+    string city;
+    string province;
+    int? zip;
+
+    //try and catch every string
+    try { name = array[0];}
+    catch { name = "undefined"; }
+
+    try { surname = array[1]; }
+    catch { surname = "undefined"; }
+
+    try { street = array[2]; }
+    catch { street = "undefined"; }
+
+    try { city = array[3]; }
+    catch { city = "undefined"; }
+
+    try { province = array[4]; }
+    catch { province = "undefined"; }
+
+    try { zip = Convert.ToInt32(array[5]); }
+    catch { zip = null; }
+
+    //new User and add to list
+    var user = new User(name, surname, street, city, province, zip);
+    users.Add(user);
+}
+
+foreach (var user in users)
+{
+    Console.WriteLine(user.ToString());
+}
